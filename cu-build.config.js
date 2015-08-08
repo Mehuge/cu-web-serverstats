a7c1302bb5e5327adf4ed63308aee917b229b66f
Mehuge <adf@sorcerer.co.uk>
2015-08-08 12:18:53 +0100

    trying various configs

--- cu-build.config.js
+++ cu-build.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   type: 'component',
   path: __dirname,
-  main: 'src/main.js'
+  legacy: true,
+  main: 'src/main.js',
+  exclude: [
+    'node_modules',
+    'dist'
+  ],
+  glob: {
+      js: ['src/*.js', 'src/*.jsx'],
+      stylus: [ 'src/*.styl', 'src/*.css' ]
+  },
+  dir: {
+    publish: 'dist'
+  }
 };
