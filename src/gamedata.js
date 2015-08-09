452c83f0e9304e0b38b2ee24787a5f3f744ff1bf
Mehuge <adf@sorcerer.co.uk>
2015-08-09 02:35:09 +0100

    Handle failed requests

--- src/gamedata.js
+++ src/gamedata.js
@@ -5,13 +5,16 @@ var GameData = function(server) {
 };
 var timer;
 GameData.prototype.poll = function(callback) {
+    function rejected(e) {
+        callback({ type: e });
+    }
     timer = setInterval(function(){
         Rest.getControlGame({ includeControlPoints: false }).then(function(args) {
             callback({ type: "controlgame", data: args });
-        });
+        }, rejected);
         Rest.getPlayers().then(function(args) {
             callback({ type: "population", data: args });
-        });
+        }, rejected);
     },1000);
 };
 GameData.prototype.cancel = function() {
