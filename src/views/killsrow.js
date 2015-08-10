af07a22666d8614c0fc56725b897d298fc52ef6b
Mehuge <adf@sorcerer.co.uk>
2015-08-10 22:10:50 +0100

    Display ratios to 2 decimal places always

--- src/views/killsrow.js
+++ src/views/killsrow.js
@@ -5,20 +5,18 @@ var KillsRow = React.createClass({
         var rest, cols = [], columns = this.props.columns;
         if (columns) {
             for (var i = 0; i < columns.length; i++) {
-                var value;
+                var value, props = this.props, info = props.info, player = info.player;
                 switch(columns[i].title) {
-                    case "Rank": value = this.props.rank; break;
-                    case "Name": value = this.props.name; break;
-                    case "Realm": value = this.props.info.player.faction; break;
-                    case "Race": value = this.props.info.player.race; break;
-                    case "Arch.":
-                        value = this.props.info.player.archetype;
-                        break;
-                    case "Kills": value = this.props.info.kills.length; break;
-                    case "Deaths": value = this.props.info.deaths.length; break;
+                    case "Rank": value = props.rank; break;
+                    case "Name": value = props.name; break;
+                    case "Realm": value =player.faction; break;
+                    case "Race": value = player.race; break;
+                    case "Arch.": value = player.archetype; break;
+                    case "Kills": value = info.kills.length; break;
+                    case "Deaths": value = info.deaths.length; break;
                     case "KDR":
-                        var kills = this.props.info.kills, deaths = this.props.info.deaths;
-                        value = deaths.length ? (kills.length/deaths.length).toFixed(2) : kills.length;
+                        var kills = info.kills, deaths = info.deaths;
+                        value = deaths.length ? (kills.length/deaths.length).toFixed(2) : kills.length.toFixed(2);
                         break;
                 }
                 var col = (<span className={columns[i].className} style={{ width: columns[i].width }}>{value}</span>);
