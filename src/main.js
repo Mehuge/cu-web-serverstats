81ea99231a26b3f7c8bd985f846eac857bcfc2c2
Mehuge <adf@sorcerer.co.uk>
2015-08-10 18:45:21 +0100

    Switch testing back to hatchery.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ var App = require('./app.js');
 // Create and launch the application
 var app = new App({
     container: document.getElementById("server-stats-container"),
-    server: "wyrmling"
+    server: "hatchery"
 });
 app.run();
